caf7b9e4e542f5372fc3a40a1402dee9441f4901
qbobl5 <ddddd991125@gmail.com>
2024-09-05 10:52:48 +0900

    240904 백준 1764 java

--- 김혜빈/BOJ/B1764.java
+++ 김혜빈/BOJ/B1764.java
@@ -26,7 +26,7 @@ public class B1764 {
             if(set.contains(s)) list.add(s); // 만약 보도 못한 사람이 듣도 못한 사람이면 list 에 추가
         }
 
-        Collections.sort(list); // 정렬
+        Collections.sort(list);
         sb.append(list.size()).append("\n");
         for(String s : list) sb.append(s).append("\n");
 
