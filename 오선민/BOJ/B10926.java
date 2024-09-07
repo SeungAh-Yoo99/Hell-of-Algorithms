b626a4498e6a72335cad361d91643b22fa700d5e
seonmin5 <seonmin5555@gmail.com>
2024-09-07 13:24:26 +0900

    240907 BOJ 10926 java

---
+++ 오선민/BOJ/B10926.java
@@ -0,0 +1,18 @@
+/**
+ * 문제 링크: https://www.acmicpc.net/problem/10926
+ * 시간:  168 ms
+ * 메모리:  17728 KB
+ * pr규칙: yymmdd 사이트명 문제번호 언어
+ */
+
+package BOJ;
+
+import java.util.Scanner;
+
+public class B10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+        System.out.println(id + "??!");
+    }
+}
