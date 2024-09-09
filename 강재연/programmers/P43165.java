2f61edc98c4241526fd1c6f4e431f48d8a316c7f
riverkite0708 <0908xmrqkr@gmail.com>
2024-09-09 18:42:24 +0900

    Update P43165.java

--- 강재연/programmers/P43165.java
+++ 강재연/programmers/P43165.java
@@ -3,9 +3,9 @@
 // 메모리: 79.6MB
 
 class Solution {
-    static int cnt = 0;
-    static int[] _numbers;
-    static int _target;
+    int cnt = 0;
+    int[] _numbers;
+    int _target;
     
     public int solution(int[] numbers, int target) {
         // 입력을 전역변수에 할당 
@@ -16,7 +16,7 @@ class Solution {
         return cnt;
     }
     
-    public static void targetNumber(int index, int sum){
+    public void targetNumber(int index, int sum){
         // index가 배열의 끝에 닿으면 재귀에서 벗어남
         if(index == _numbers.length){
             // 그렇게 끝까지 갔을때 _target과 sum이 같으면 cnt 증가
@@ -27,7 +27,7 @@ class Solution {
         }
         
         // 누적합을 구하되 다음 index 값을 더하는 경우와 빼는 경우로 분기를 나눔
-        dfs(index + 1, sum + _numbers[index]);
-        dfs(index + 1, sum - _numbers[index]);
+        targetNumber(index + 1, sum + _numbers[index]);
+        targetNumber(index + 1, sum - _numbers[index]);
     }
 }
