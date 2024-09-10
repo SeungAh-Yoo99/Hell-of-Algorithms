2ed7596783504cc6eb42b45a150ed4c7a6873b69
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-09-10 23:26:43 +0900

    240910 백준 1267.java

---
+++ 이도이/BOJ/1267.java
@@ -0,0 +1,49 @@
+// 문제 링크:https://www.acmicpc.net/problem/1267
+// 시간: 96ms
+// 메모리: 14120KB
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+     public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            int young = 0;
+            int min = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i =0; i<n; i++){
+              int a = Integer.parseInt(st.nextToken());
+              young += 10;
+              min += 15;
+              if(a/30 >= 1 ){
+                young += (10*(a/30));
+              }else{
+                young += 0;
+              }
+
+              if(a/60 >= 1 ){
+                min += (15*(a/60));
+              }else{
+                min += 0;
+              }
+
+            }
+
+            if(young < min){
+              sb.append("Y").append(" ").append(young);
+            }else if(young > min){
+                sb.append("M").append(" ").append(min);
+            }else{
+              sb.append("Y").append(" ").append("M").append(" ").append(min);
+            }
+
+             System.out.println(sb);
+     }
+
+
+}
