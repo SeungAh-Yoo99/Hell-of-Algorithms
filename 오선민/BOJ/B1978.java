a13090e78f91aed63a5c5070f968999c5dfe49a8
seonmin5 <seonmin5555@gmail.com>
2024-09-15 15:53:59 +0900

    240915 백준 1978 java

--- 오선민/BOJ/B1978.java
+++ 오선민/BOJ/B1978.java
@@ -1,15 +1,19 @@
 //문제 링크: https://www.acmicpc.net/problem/1978
-//시간: 172 ms
-//메모리: 17804 KB
+//시간:  96ms
+//메모리:  14152KB
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class B1978 {
     //  소수 여부 확인
     public static boolean isPrime(int number) {
         if (number <= 1) {
             return false;
         } else {
-            for (int j=2; j<number; j++) {
+            for (int j=2; j*j<=number; j++) {
                 if (number % j == 0) {
                     return false;
                 }
@@ -18,20 +22,17 @@ public class B1978 {
         return true;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
 
-        String[] inputs = sc.nextLine().split(" ");
-        sc.close();
-
-        int[] numbers = new int[n]; // inputs String -> int
         int count = 0;
 
-        for (int i=0; i<n; i++) {
-            numbers[i] = Integer.parseInt(inputs[i]);
-            if (isPrime(numbers[i])) {
+        while (st.hasMoreTokens()) {
+            int number = Integer.parseInt(st.nextToken());
+            if (isPrime(number)) {
                 count++;
             }
         }
