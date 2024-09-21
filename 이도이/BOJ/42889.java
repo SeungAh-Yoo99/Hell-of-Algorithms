634840f7bb623b3b87c31df100ce096c7ced25a5
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-09-22 03:09:24 +0900

    240921 프로그래머스 42889.java
    흠 이 문제 자바스크립트로 풀어봤어서 쉽게 풀릴 줄 알았는데,,, 엄청나네요 자바의 세계란 intValue()메소드도 처음 알았습니다! 기본 타입 배열은 Comparator를 사용할 수 없다!! 명심,,

--- 이도이/BOJ/42889.java
+++ 이도이/BOJ/42889.java
@@ -1,22 +1,53 @@
 문제 링크:https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java
-시간: ms
-메모리: KB
+시간: 5.59ms
+메모리: 86.3MB
+
+import java.util.*;
 
 class Solution {
-    public int solution(int N, int[] stages) {
-        int[] result = new int[N];
-        int[] array = new int[stages.length-1];
-        for(int i=0; i<stages.length; i++){
-            array[stages[i]-1]++;
+    public int[] solution(int N, int[] stages) {
+          // 실패율과 인덱스값 매치
+          double[][] result = new double[N][2];
+          // 분자값
+          int[] arr = new int[N];
+          // 분모값 계산을 위한 sum
+          int sum = 0;
+          // 정답
+          int[] answer = new int[N];
+        
+         // 분자값
+         for(int i=0; i<stages.length; i++){
+             if(stages[i] != N+1){
+                arr[stages[i]-1]++; 
+              }
+          }
+        
+        // 실패율 계산
+         for(int i=0; i<result.length; i++){
+             if(stages.length-sum > 0){
+               result[i][0] = (double)arr[i]/(stages.length-sum);        
+             }else{
+                 result[i][0] = 0;
+             }
+              result[i][1] = i+1;
+              sum += arr[i];
+        }
+
+        // 람다식을 이용할 내림차순 정렬을 위해 Double[][]로 교체 
+        Double[][] boxedResult = new Double[N][2];
+        for (int i = 0; i < N; i++) {
+            boxedResult[i][0] = result[i][0];
+            boxedResult[i][1] = result[i][1];
+        }
+
+        Arrays.sort(boxedResult, (a,b) -> Double.compare(b[0],a[0]));
+    
+        // 정답을 출력하기 위해 2차원 배열에서 인데스값만 추출
+        for(int i=0; i<N; i++){
+           answer[i] = boxedResult[i][1].intValue();
         }
         
-        for(int i=0; i<array.length-1; i++){
-           array[i] = array[i]/(stages.length-array[i]);
-        }
- 
-        Arrays.sort(array, Collections.reverseOrder());
-     
-        return array;
+        return  answer;
         
     }
 }
