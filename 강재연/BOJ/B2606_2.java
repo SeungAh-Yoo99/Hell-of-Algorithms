6de2a5f7e51d27d26d7b382bebf4c5833a0f0a1c
riverkite0708 <0908xmrqkr@gmail.com>
2024-09-25 18:54:40 +0900

    240925 백준 2606_2 java

---
+++ 강재연/BOJ/B2606_2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class boj2606 {
+	static int[][] graph;
+	static boolean[] visited;
+	static int cnt = 0;
+	static int N;
+	static int M;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		graph = new int[N][N];
+		visited = new boolean[N];
+		StringTokenizer st;
+				
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken())-1;
+			int B = Integer.parseInt(st.nextToken())-1;
+			graph[A][B] = 1;
+			graph[B][A] = 1;
+		}
+		for(int k=0; k<graph.length; k++) {
+			System.out.println(Arrays.toString(graph[k]));
+		}
+		dfs(0);
+		System.out.println(cnt);
+	}
+	
+	static void dfs(int index) {
+		visited[index] = true;
+		System.out.println(index + " -> ");
+		for(int i=0; i<N; i++) {
+			if(graph[index][i]==1 && !visited[i]) {
+				cnt++;
+				dfs(i);
+			}
+		}
+	}
+}
