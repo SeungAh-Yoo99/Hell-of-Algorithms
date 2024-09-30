ae8bf882f1944b20886147558e5b085528f99a54
yaejinkong <0910a@naver.com>
2024-09-30 12:14:10 +0900

    240928 백준 1966 java

--- 공예진/BOJ/B1966.java
+++ 공예진/BOJ/B1966.java
@@ -1,6 +1,6 @@
 //문제 링크: https://www.acmicpc.net/problem/1966
-//시간: ms
-//메모리: KB
+//시간: 84ms
+//메모리: 12588KB
 
 import java.util.*;
 import java.io.*;
@@ -13,27 +13,46 @@ public class Main {
 
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken()); // 문서 개수
+            int M = Integer.parseInt(st.nextToken()); // 인덱스
 
             Queue<int[]> queue = new LinkedList<>();
 
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                queue.add(new int[] {j, num});
+                queue.add(new int[] {j, num}); // 인덱스, 중요도
             }
 
-            while (!queue.isEmpty()) {
+            int cnt = 0; // 문서의 출력 순서
+
+            while (true) {
+                boolean hasHigherPriority = false; // 자신보다 중요도가 높은 문서가 있는 지 확인하는 용도
                 int[] selected = queue.poll();
 
                 for (int[] arr : queue) {
-                    if (selected[1] < arr[1]) {
-                        queue.add(selected);
+                    if (selected[1] < arr[1]) { // 중요도 비교
+                        hasHigherPriority = true;
                         break;
                     }
                 }
+
+                if (hasHigherPriority) { // 자신보다 중요도가 높은 문서가 있는 경우
+                    queue.add(selected);
+                } else { // 자신이 가장 높은 중요도를 가진 경우
+                    if (selected[0] == M) { // 인덱스 비교
+                        cnt++;
+                        break;
+                    } else {
+                        cnt++;
+                    }
+                }
             }
+            System.out.println(cnt);
         }
     }
 }
+
+
+
+
