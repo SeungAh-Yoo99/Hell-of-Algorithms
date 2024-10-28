1c8acc271a9bfc828db6babc6da283ca2b1148e7
Jaeyeon Kang <0908xmrqkr@gmail.com>
2024-10-29 08:40:46 +0900

    241029 프로그래머스 12909 java

---
+++ 강재연/programmers/P12909.js
@@ -0,0 +1,17 @@
+// 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12909
+// 시간: 0.05ms
+// 메모리: 33.6MB
+
+function solution(s){
+    let bracket = [...s];
+    let balance = 0;
+    bracket.forEach((b, i) => {
+        if(balance < 0) return false;
+        
+        if(b == "(") balance++;
+        else balance --;
+    });
+    
+    if(balance == 0) return true;
+    else return false;
+}
