e107ceb8c1ab654fc69cb115805387ed86855adf
yaejinkong <0910a@naver.com>
2024-11-10 09:16:02 +0900

    241107 프로그래머스 12915 java

--- 공예진/programmers/P12915.java
+++ 공예진/programmers/P12915.java
@@ -1,18 +1,25 @@
 //문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12915
-//시간:
-//메모리:
+//시간: 0.61ms
+//메모리: 73.7MB
+
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length + 1];
-
+        String[] answer = new String[strings.length];
         for (int i = 0; i < strings.length; i++) {
-            if (i == 0) {
-                answer[i] = strings[i];
-            } else {
-                char ch = strings[i].charAt(n);
-                if (answer[i - 1].charAt(n) < ch) {
-                    answer[i] = strings[i];
+            char ch = strings[i].charAt(n);
+            answer[i] = Character.toString(ch);
+        }
+
+        Arrays.sort(answer);
+        Arrays.sort(strings);
+        for (int i = 0; i < strings.length; i++) {
+            String st = Character.toString(strings[i].charAt(n));
+            for (int j = 0; j < answer.length; j++) {
+                if (st.equals(answer[j])) {
+                    answer[j] = strings[i];
+                    break;
                 }
             }
         }
