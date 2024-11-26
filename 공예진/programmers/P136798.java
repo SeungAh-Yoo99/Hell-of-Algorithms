807a9f2306e756f2e43fcfbd442d80922e48fb5b
yaejinkong <0910a@naver.com>
2024-11-27 08:57:15 +0900

    241122 프로그래머스 136798 java (~ing)

---
+++ 공예진/programmers/P136798.java
@@ -0,0 +1,35 @@
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/136798
+//시간:
+//메모리:
+//정확성: 66.7
+//합계: 66.7 / 100.0
+
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] arr = new int[number];
+
+        for (int i = 1; i <= number; i++) {
+            int cnt = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    cnt++;
+                }
+            }
+
+            if (cnt > limit) {
+                cnt = power;
+            }
+            arr[i - 1] = cnt;
+        }
+
+        for (int num : arr) {
+            answer += num;
+        }
+
+        return answer;
+    }
+}
