9f85c8d9d775d9b2db6f55eb588191144c9148fe
yaejinkong <0910a@naver.com>
2025-01-17 02:38:06 +0900

    250116 프로그래머스 42587(refactoring) java

---
+++ 공예진/programmers/P42587(2).java
@@ -0,0 +1,40 @@
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181908
+//시간: 19.48ms
+//메모리: 79.3MB
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<String> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            String s = priorities[i] + "," + i;
+            queue.offer(s);
+        }
+
+        Integer[] arr = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, Collections.reverseOrder()); // 내림차순 정렬
+
+        int idx = 0;
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            String str = queue.poll();
+            String[] strArr = str.split(",");
+            int priority = Integer.parseInt(strArr[0]); // 우선순위
+            int qLocation = Integer.parseInt(strArr[1]); // 위치
+
+            if (priority == arr[idx]) {
+                idx++;
+                answer++;
+                if (location == qLocation) {
+                    break;
+                }
+            } else {
+                queue.offer(str);
+            }
+        }
+
+        return answer;
+
+    }
+}
