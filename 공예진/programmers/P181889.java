296eaf12297de6f26e6f5ad856525a4f64f1f1b3
yaejinkong <0910a@naver.com>
2025-01-22 18:04:04 +0900

    250122 프로그래머스 181889 java

---
+++ 공예진/programmers/P181889.java
@@ -0,0 +1,11 @@
+//문제: https://school.programmers.co.kr/learn/courses/30/lessons/181889
+//시간: 0.05ms
+//메모리: 74.1MB
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, 0, n);
+    }
+}
