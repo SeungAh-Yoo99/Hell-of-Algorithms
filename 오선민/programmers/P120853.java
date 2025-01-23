948aec97a680b3dc3ae63bb5b3c78bf7ce112e9a
seonmin5 <seonmin5555@gmail.com>
2025-01-23 13:45:21 +0900

    250121 프로그래머스 120853 java 문제풀이방법추가

--- 오선민/programmers/P120853.java
+++ 오선민/programmers/P120853.java
@@ -4,11 +4,11 @@
  * 메모리: 85.1MB
  * pr규칙: yymmdd 사이트명 문제번호 언어
  */
-
-
 package programmers;
+import java.util.*;
 
 public class P120853 {
+    // 기본 풀이방법
     public int solution(String s) {
         String arr[] = s.split(" ");
         int result = 0;
@@ -19,6 +19,27 @@ public class P120853 {
             } else {
                 result += Integer.parseInt(arr[i]);
             }
+        }
+
+        return result;
+    }
+
+    // 스택을 사용한 방법
+    public int solution2(String s) {
+        String arr[] = s.split(" ");
+        Stack<Integer> stack = new Stack<>();
+
+        for (String str : arr) {
+            if (str.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(str));
+            }
+        }
+
+        int result = 0;
+        for (int n: stack) {
+            result += n;
         }
 
         return result;
