c6e94a794373d041162cfd39d5a66353838fe653
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-01-25 21:14:24 +0900

    250118 프로그래머스 12941.js

--- 이도이/programmers/12941.js
+++ 이도이/programmers/12941.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12941
-시간:0.18ms
-메모리:33.5MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12941
+//시간:0.18ms
+//메모리:33.5MB
 
 function solution(A,B){
     A.sort((a,b) => a-b);
