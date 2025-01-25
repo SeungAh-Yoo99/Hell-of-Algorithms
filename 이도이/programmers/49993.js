5b1e57659c31e2054d012d367883a221e7e8221d
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-01-25 21:17:49 +0900

    250117 프로그래머스 49993 js

--- 이도이/programmers/49993.js
+++ 이도이/programmers/49993.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/49993
-시간:5.07ms
-메모리:  33.7MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/49993
+//시간:5.07ms
+//메모리:  33.7MB
 
 function solution(skill, skill_trees) {
     let object = {};
