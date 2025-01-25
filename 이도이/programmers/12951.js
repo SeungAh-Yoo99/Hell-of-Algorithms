f5c18ad697b5ae0026feb4358094257c69fda5a0
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-01-25 21:22:34 +0900

    250116 프로그래머스 12951.js

--- 이도이/programmers/12951.js
+++ 이도이/programmers/12951.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12951
-시간: 0.30ms
-메모리: 33.5MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12951
+//시간: 0.30ms
+//메모리: 33.5MB
 
 function solution(s) {
     let arrs = s.split(" ")
