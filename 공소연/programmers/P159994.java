4d158e4d2642831c3830998091fbad9f5a144a82
Soyeon Kong <87259219+Kong-E@users.noreply.github.com>
2025-02-02 08:35:25 +0900

    Update P159994.java
    풀이 수정

--- 공소연/programmers/P159994.java
+++ 공소연/programmers/P159994.java
@@ -1,38 +1,36 @@
 // 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=java
-// 시간: 0.04ms
-// 메모리: 80.6MB
+// 시간: 0.02ms
+// 메모리: 91.6MB
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String result = "No";
-        int idx = 0;
+        int lastIdx1 = 0;
         int lastIdx2 = 0;
-        
+
         for (int idx1 = 0; idx1 < goal.length; idx1++) {
             boolean found = false;
-            
+
             // cards1에서 goal[idx1]을 찾기
-            if (idx < cards1.length && cards1[idx].equals(goal[idx1])) {
-                idx++;
+            if (lastIdx1 < cards1.length && cards1[lastIdx1].equals(goal[idx1])) {
+                lastIdx1++;
                 found = true;
             }
-            
+
             // cards2에서 goal[idx1]을 찾기
             if (!found) {
-                for (int idx2 = lastIdx2; idx2 < cards2.length; idx2++) {
-                    if (cards2[idx2].equals(goal[idx1])) {
-                        lastIdx2 = idx2 + 1; 
-                        found = true;
-                        break;
-                    }
+                if (lastIdx2 < cards2.length && cards2[lastIdx2].equals(goal[idx1])) {
+                    lastIdx2++;
+                    found = true;
                 }
             }
             
-            if (!found) {
+            
+            if (!found) { // cards2에서도 못찾았을 때
                 return result;
             }
         }
-        
+
         return "Yes"; 
     }
 }
