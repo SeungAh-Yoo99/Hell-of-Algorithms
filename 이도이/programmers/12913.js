b7aaf8dd40dd00c9aa5ccf6a840b20b6d81b985e
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:48:42 +0900

    Update and rename 이도이/12913 js to 이도이/programmers/12913.js

--- 이도이/programmers/12913.js
+++ 이도이/programmers/12913.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12913
-시간: 2.13ms
-메모리:34.1MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12913
+//시간: 2.13ms
+//메모리:34.1MB
 
 
 
