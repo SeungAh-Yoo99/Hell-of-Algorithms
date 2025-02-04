e9bdf3f97050fe0049ed3788a03ef726e625545d
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:13:32 +0900

    Update and rename 12947 js to 12947.js

--- 이도이/programmers/12947.js
+++ 이도이/programmers/12947.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12947
-시간: 0.05ms
-메모리: 33.4MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12947
+//시간: 0.05ms
+//메모리: 33.4MB
 
 function solution(x) {    
     let numArr = x.toString().split("").map(Number);
