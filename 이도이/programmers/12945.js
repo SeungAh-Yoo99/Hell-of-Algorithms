498a5a687f3c3596deade6817a20b1d359414ee8
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:13:19 +0900

    Update and rename 12945 javascript to 12945.js

--- 이도이/programmers/12945.js
+++ 이도이/programmers/12945.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12945
-시간: 7.10ms
-메모리: 38.9MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12945
+//시간: 7.10ms
+//메모리: 38.9MB
 
 function solution(n) {
     let f = [0, 1]
