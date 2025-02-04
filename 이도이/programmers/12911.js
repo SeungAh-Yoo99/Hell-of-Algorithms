f0c1edf33011bc5ccd16503b35303a00282cd2ab
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:11:27 +0900

    Update and rename 12911 javascript to 12911.js

--- 이도이/programmers/12911.js
+++ 이도이/programmers/12911.js
@@ -1,6 +1,6 @@
-문제 링크:https://school.programmers.co.kr/learn/courses/30/lessons/12911
-시간: 0.22ms
-메모리: 33.1MB
+//문제 링크:https://school.programmers.co.kr/learn/courses/30/lessons/12911
+//시간: 0.22ms
+//메모리: 33.1MB
 
 
 function solution(n) {
