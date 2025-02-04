08284a17b63f8d8e1ad23386922f3e112b5dfde4
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:13:47 +0900

    Update and rename 12953 javascript to 12953.js

--- 이도이/programmers/12953.js
+++ 이도이/programmers/12953.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12953
-시간: 0.12ms
-메모리: 33.4MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12953
+//시간: 0.12ms
+//메모리: 33.4MB
 
 // 최소공배수 구하기 num1 * num2 / num1와num2의 최대공약수
 function solution(arr) {   
