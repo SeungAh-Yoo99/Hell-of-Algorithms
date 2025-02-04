13587707a0b4e5c0744251eb275261cae1d81c2b
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:14:03 +0900

    Update and rename 12973 javascript to 12973.js

--- 이도이/programmers/12973.js
+++ 이도이/programmers/12973.js
@@ -1,6 +1,6 @@
-문제 링크:https://school.programmers.co.kr/learn/courses/30/lessons/12973
-시간: 33.05ms
-메모리: 40MB
+//문제 링크:https://school.programmers.co.kr/learn/courses/30/lessons/12973
+//시간: 33.05ms
+//메모리: 40MB
 
 function solution(s)
 {
