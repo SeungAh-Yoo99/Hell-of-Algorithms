f9edcd66ab1303b1c3b1f3751e3bc2bb56241c93
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:11:59 +0900

    Update and rename 12924 javascript to 12924.js

--- 이도이/programmers/12924.js
+++ 이도이/programmers/12924.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12924
-시간: 0.19ms
-메모리: 33.5MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12924
+//시간: 0.19ms
+//메모리: 33.5MB
 
 function solution(n) {
     let sum=1, right=1;
