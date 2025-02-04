8b21b4ad0ed7c1bc73fba9e2dbd693e9978f1ce7
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-04 18:14:19 +0900

    Update and rename 12985 js to 12985.js

--- 이도이/programmers/12985.js
+++ 이도이/programmers/12985.js
@@ -1,6 +1,6 @@
-문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12985
-시간: 0.18ms
-메모리: 33.4MB
+//문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12985
+//시간: 0.18ms
+//메모리: 33.4MB
 
 function solution(n,a,b){
     let answer = 1;
